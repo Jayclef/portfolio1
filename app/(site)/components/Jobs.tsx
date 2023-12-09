39722f8404b5147f293378b930f3b82a9951c193
Jayclef <mbajames346@gmail.com>
2023-12-09 07:35:53 +0100

    worked on jobs

--- app/(site)/components/Jobs.tsx
+++ app/(site)/components/Jobs.tsx
@@ -8,7 +8,7 @@ export default async function Job() {
   return (
     <section className="mt-6 py-8 dark:bg-black">
       <div className="mb-16">
-        <h2 className="font-semibold text-4xl mb-4">Work Experience</h2>
+        <h2 className="dark:text-[#ccc] font-semibold text-4xl mb-4">Work Experience</h2>
       </div>
 
       <div className="flex flex-col gap-y-12">
@@ -30,8 +30,8 @@ export default async function Job() {
               />
             </a>
             <div className="flex flex-col items-start">
-              <h3 className="text-xl font-bold">{data.name}</h3>
-              <p>{data.jobTitle}</p>
+              <h3 className="text-xl font-bold dark:text-[#ccc]">{data.name}</h3>
+              <p className="dark:text-[#ccc]">{data.jobTitle}</p>
               <small className="text-sm text-zinc-500 mt-2 tracking-widest uppercase">
                 {data.startDate.toString()} - {data.endDate.toString()}
               </small>
